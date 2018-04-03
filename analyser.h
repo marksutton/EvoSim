9668f244b2cd09ee42d7d7845cae1db02702170b
Alan Spencer <a.r.t.spencer@gmail.com>
2018-04-03 11:16:29 +0100

    Added Copyright/Licence headers to all files
    also updated the README file.

--- analyser.h
+++ analyser.h
@@ -1,3 +1,20 @@
+/**
+ * @file
+ * Header: Analyser
+ *
+ * All REVOSIM code is released under the GNU General Public License.
+ * See GNUv3License.txt files in the programme directory.
+ *
+ * All REVOSIM code is Copyright 2018 by Mark Sutton, Russell Garwood,
+ * and Alan R.T. Spencer.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or (at
+ * your option) any later version. This program is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY.
+ */
+
 #ifndef ANALYSER_H
 #define ANALYSER_H
 #include <QString>
